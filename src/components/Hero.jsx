e270ae12ca416cd3554b2ae13bbb3fc69f3bfea5
Shreyanshs0ni <shreyanshsoni07@gmail.com>
2024-11-15 00:18:02 +0530

    final commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,40 @@
 import "./Hero.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAmazon } from "@fortawesome/free-brands-svg-icons";
+import { faEbay } from "@fortawesome/free-brands-svg-icons";
+import Shoes from "../assets/Shoes.png";
 const Hero = () => {
   return (
     <main className="hero">
       <div className="hero-content">
         <h1>YOUR FEET DESERVE THE BEST</h1>
-        <p>YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES. YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.</p>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES.
+          YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES.
+        </p>
+        <div className="hero-btn">
+          <button className="btn">SHOP NOW</button>
+          <button className="sec-btn">CATEGORY</button>
+        </div>
+        <div className="shop-on">
+          <p>Also Available On</p>
+          <div className="font-awe">
+            <FontAwesomeIcon
+              icon={faAmazon}
+              style={{ color: "#ffffff" }}
+              className="font-icon"
+            />
+            <FontAwesomeIcon
+              icon={faEbay}
+              style={{ color: "#ffffff" }}
+              className="font-icon"
+            />
+          </div>
+        </div>
       </div>
-      <div className="hero-img"></div>
+      <div className="hero-img">
+        <img src={Shoes} alt="" />
+      </div>
     </main>
   );
 };
